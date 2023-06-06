492cc2e98bf05988b2f33c56df76baec40c238ca
roman-ojha <razzroman98@gmail.com>
2023-06-06 10:47:28 -0700

    feat: added cart view blade template

--- resources/views/layout/base.blade.php
+++ resources/views/layout/base.blade.php
@@ -155,9 +155,13 @@
                     <div class="header__cart">
                         <ul>
                             <li><a href="#"><i class="fa fa-heart"></i> <span>1</span></a></li>
-                            <li><a href="#"><i class="fa fa-shopping-bag"></i> <span>3</span></a></li>
+                            <li><a href="#"><i class="fa fa-shopping-bag"></i>
+                                    <span>{{ Cart::name('shopping')->getDetails()->get('items_count') }}</span></a>
+                            </li>
                         </ul>
-                        <div class="header__cart__price">item: <span>$150.00</span></div>
+                        <div class="header__cart__price">item:
+                            <span>{{ Cart::name('shopping')->getDetails()->get('total') }}</span>
+                        </div>
                     </div>
                 </div>
             </div>
