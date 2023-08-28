b3838d1d0ce9e23b9ef1fd0ec6fa18b4df58f4d2
Roman-Ojha <razzroman98@gmail.com>
2023-08-28 22:26:09 +0545

    feat: improved checkout page as per required

--- resources/views/checkout.blade.php
+++ resources/views/checkout.blade.php
@@ -14,14 +14,8 @@
 
 @section('content')
     <!-- Checkout Section Begin -->
-    <section class="checkout spad">
+    <section class="checkout spad"style="padding-bottom: 20px;padding-top: 40px">
         <div class="container">
-            <div class="row">
-                <div class="col-lg-12">
-                    <h6><span class="icon_tag_alt"></span> Have a coupon? <a href="#">Click here</a> to enter your code
-                    </h6>
-                </div>
-            </div>
             @if ($errors->any())
                 <div class="alert alert-danger">
                     @foreach ($errors->all() as $error)
@@ -56,37 +50,10 @@
                                 <input type="text" name="country" value="{{ old('country') }}" />
                                 <small>{{ $errors->first('country') }}</small>
                             </div>
-                            <div class="checkout__input">
-                                <p>Address<span>*</span></p>
-                                <input type="text" placeholder="Street Address" class="checkout__input__add"
-                                    name="address" value="{{ old('address') }}" />
-
-                            </div>
-                            <div class="checkout__input">
-                                <p>District<span>*</span></p>
-                                <input type="text" name="district" value="{{ old('district') }}" />
-                            </div>
-                            <div class="checkout__input">
-                                <p>Province<span>*</span></p>
-                                <input type="text" name="province" value="{{ old('province') }}" />
-                            </div>
-                            <div class="checkout__input">
-                                <p>Postcode / ZIP<span>*</span></p>
-                                <input type="text" name="zip" value="{{ old('zip') }}" />
-                            </div>
-                            <div class="row">
-                                <div class="col-lg-6">
-                                    <div class="checkout__input">
-                                        <p>Phone<span>*</span></p>
-                                        <input type="text" name="phone" value="{{ old('phone') }}" />
-                                    </div>
-                                </div>
-                                <div class="col-lg-6">
-                                    <div class="checkout__input">
-                                        <p>Email<span>*</span></p>
-                                        <input type="text" name="email" value="{{ old('email') }}" />
-                                    </div>
-                                </div>
+                            <div class="checkout__input @if ($errors->has('country')) invalid @endif">
+                                <p>Email<span>*</span></p>
+                                <input type="text" name="email" value="{{ old('email') }}" />
+                                <small>{{ $errors->first('email') }}</small>
                             </div>
                             <div class="checkout__input">
                             </div>
@@ -103,14 +70,14 @@
                                 <div class="checkout__order__subtotal">Subtotal <span>Rs. {{ $subTotal }}</span></div>
                                 <div class="checkout__order__total">Total <span>Rs. {{ $total }}</span></div>
 
-                                <div class="checkout__input__checkbox">
+                                {{-- <div class="checkout__input__checkbox">
                                     <label for="payment">
                                         Cash on Delivery
                                         <input type="radio" id="payment" name="payment_gateway" value="cod"
                                             @if (old('payment_gateway') == 'cod') checked @endif />
                                         <span class="checkmark"></span>
                                     </label>
-                                </div>
+                                </div> --}}
                                 <div class="checkout__input__checkbox">
                                     <label for="khalti">
                                         Khalti
